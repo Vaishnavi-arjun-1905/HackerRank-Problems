a168bb54ba11b3f51c88801cd97e97ee02ca5a97
Vaishnavi-Arjun-1905 <vaishnavia@600@gmail.com>
2025-02-06 00:00:48 +0530

    added swap two num file

---
+++ swap-two-numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    int b;
+    
+    scanf("%d",&a);
+    scanf("%d",&b);
+    
+    int temp =a;
+    
+    a=b;
+    b=temp;
+    
+    printf("a = %d\n",a);
+    printf("b = %d\n",b);
+      
+    return 0;
+}
